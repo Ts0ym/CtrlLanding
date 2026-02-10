e2afa8b989ad9ff203741247289c37d44db3784f
T0kyto <tsoymaksimi@mail.ru>
2026-02-11 06:20:52 +1000

    changes in config

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,10 @@
 /** @type {import('next').NextConfig} */
 
-// GitHub Pages: site is at https://username.github.io/REPO_NAME/
-const isGitHubPages = process.env.GITHUB_ACTIONS && process.env.GITHUB_REPOSITORY;
-const repoName = isGitHubPages ? process.env.GITHUB_REPOSITORY.split("/")[1] : "";
-const basePath = repoName ? `/${repoName}` : "";
-const assetPrefix = basePath ? `${basePath}/` : "";
-
 const nextConfig = {
   output: "export",
   distDir: "output",
-  basePath,
-  assetPrefix,
+  basePath: "/CtrlLanding",
+  assetPrefix: "/CtrlLanding/",
   devIndicators: {
     buildActivity: false,
   },
